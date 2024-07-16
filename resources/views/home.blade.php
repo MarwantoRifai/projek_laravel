7f64feeda549bda3265374621d7748f90513deb6
MarwantoRifai <mrrifai027@gmail.com>
2024-07-17 06:45:39 +0700

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,60 @@
+@extends('theme.header')
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>HOME</title>
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+<body>
+    <div class="container mt-3">
+    <div class="d-flex justify-content-between flex-wrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <h1 class="h1">Aplikasi Pemesanan Tiket<b>/Home</b></h1>
+        </div>
+        <div class="row">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-header bg-primary text-white">
+                        Pendaftar
+                    </div>
+                    <div class="card-body text-center">
+                        <h1>{{ $hitung1 }}</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-header bg-info text-white">
+                        Tiket
+                    </div>
+                    <div class="card-body text-center">
+                        <h1>{{ $hitung2 }}</h1>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-header bg-warning text-white">
+                        Transaksi
+                    </div>
+                    <div class="card-body text-center">
+                        <h1>{{ $hitung3 }}</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Bootstrap JS Bundle (popper.js included) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js">
+    </script>
+
+</body>
+</html>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\TransaksiController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/theme', function () {
+    return view('theme.navbar');
+});
+
+
+Route::get('Home',[HomeController::class,'Home'])->name('Home');
+Route::get('input',[HomeController::class,'input'])->name('input');
+Route::get('viewPendaftar',[HomeController::class,'viewPendaftar'])->name('viewPendaftar');
+Route::get('HalamInputTiket',[EventController::class,'HalamInputTiket'])->name('HalamInputTiket');
+Route::get('viewTiket',[EventController::class,'viewTiket'])->name('viewTiket');
+Route::post('daftar',[HomeController::class,'daftar'])->name('daftar');
+Route::post('simpanEvent',[EventController::class,'simpanEvent'])->name('simpanEvent');
+Route::get('hapus{id}',[HomeController::class,'hapus_pengguna'])->name('hapus');
+Route::get('hapus_tiket/{id}',[EventController::class,'hapus_tiket'])->name('hapus_tiket');
+Route::get('/edit_transaksi/{id}/edit', [HomeController::class, 'hapus_transaksi'])->name('hapus_transaksi');
+Route::get('viewTransaksi',[TransaksiController::class,'viewTransaksi'])->name('viewTransaksi');
+Route::get('/edit_transaksi/{id}/edit', [HomeController::class, 'edit'])->name('editTransaksi');
+Route::get('tampilkanData',[TransaksiController::class,'tampilkanData'])->name('tampilkanData');
+Route::post('saveTransaksi',[TransaksiController::class,'saveTransaksi'])->name('saveTransaksi');
+Route::get('/edit_pengguna/{id}/edit', [HomeController::class, 'edit'])->name('edit');
+Route::post('/up/{id}', [HomeController::class, 'update'])->name('up');
+Route::get('/editEvent/{id}/edit', [EventController::class, 'editEvent'])->name('editEvent');
+Route::post('/updateEvent/{id}', [EventController::class, 'updateEvent'])->name('updateEvent');
